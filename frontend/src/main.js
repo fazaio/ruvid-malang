8089264c66910c9c2766fdd0511c7e7277bfc3f3
Rengga Prakoso Nugroho <rengga.prakoso@gmail.com>
2021-08-08 13:47:07 +0700

    feat: add fontawesome 5

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -4,13 +4,19 @@ import App from './App.vue'
 import './assets/tailwind.css'
 import router from './router/index'
 
+// FontAwesome Import
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { faHospital,faAmbulance,faHeart,faAirFreshener } from "@fortawesome/free-solid-svg-icons";
+
+library.add(faHospital,faAmbulance,faHeart,faAirFreshener);
 
 axios.defaults.baseURL = 'https://ruvid-api.fronthings.com:30000/';
 
-// createApp(App).use(axios).use(router).mount('#app')
 const app = createApp(App).use(router)
+
+app.component("font-awesome-icon", FontAwesomeIcon)
 
 app.config.globalProperties.axios=axios
 
 app.mount('#app')
-
