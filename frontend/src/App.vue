b3075ddee16abc43af7b7f3eef116e99cb0a1c46
Rengga Prakoso Nugroho <rengga.prakoso@gmail.com>
2021-08-08 13:48:00 +0700

    refactor: split sites title area and basic header

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,19 +1,22 @@
 <template>
   <div class="text-gray-500 bg-gray-100">
+    <headers />
+    <pageTitles />
     <router-view />
     <footers />
   </div>
 </template>
 
 <script>
+import header from "./components/header.vue";
 import footer from "./components/footer.vue";
+import pageTitle from "./components/pageTitle.vue";
 
 export default {
   components: {
     footers: footer,
+    pageTitles: pageTitle,
+    headers: header,
   },
 };
 </script>
-
-<style>
-</style>
---
+++ frontend/src/components/pageTitle.vue
@@ -0,0 +1,13 @@
+<template>
+  <section id="page-title" class="px-8 pt-12">
+      <div class="my-0 mx-auto">
+        <!-- <font-awesome-icon icon="user-secret" /> -->
+        <h1 class="font-bold text-4xl">
+          RUVID-MALANG
+        </h1>
+        <p class="mt-4"
+          >Kumpulan Informasi untuk tanggap <b>COVID-19</b> kota Malang.</p
+        >
+      </div>
+  </section>
+</template>
