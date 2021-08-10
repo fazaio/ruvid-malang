189eab96ce7f9ccb036af28c826921421da71348
Rengga Prakoso Nugroho <rengga.prakoso@gmail.com>
2021-08-10 19:35:00 +0700

    chore: style CTA Button

--- frontend/src/components/cardLayanan.vue
+++ frontend/src/components/cardLayanan.vue
@@ -1,16 +1,16 @@
 <template>
   <div
     :id="data.url"
-    class="text-center bg-white py-8 px-8 shadow-sm rounded flex-col border-2 border-gray-200"
+    class="text-center items-center bg-white py-8 px-8 shadow-sm rounded flex flex-col border-2 border-gray-200"
   >
     <font-awesome-icon class="pb-4" :icon="data.icon" size="6x" />
     <div>
       <h2 class="font-bold text-2xl ">{{ data.title }}</h2>
-      <div class="mt-4">{{ data.description }}</div>
+      <div class="">{{ data.description }}</div>
     </div>
     <button
       @click="$router.push(data.url)"
-      class="mt-6 h-14 py-2 px-6 rounded-full bg-green-100 border-4 border-green-300 text-sm font-bold "
+      class="w-full mt-6 py-3 px-6 rounded bg-blue-400 text-white font-bold"
     >
       Selengkapnya
     </button>
