d0a1fadbb1ecedc736a4a2946da09c8e74d6c89f
Rengga Prakoso Nugroho <rengga.prakoso@gmail.com>
2021-08-10 18:04:17 +0700

    style: add border and margin on content

--- frontend/src/components/cardLayanan.vue
+++ frontend/src/components/cardLayanan.vue
@@ -1,12 +1,12 @@
 <template>
   <div
     :id="data.url"
-    class="text-center bg-white py-8 px-8 shadow-sm rounded flex-col"
+    class="text-center bg-white py-8 px-8 shadow-sm rounded flex-col border-2 border-gray-200"
   >
-    <font-awesome-icon :icon="data.icon" size="6x" />
+    <font-awesome-icon class="pb-4" :icon="data.icon" size="6x" />
     <div>
       <h2 class="font-bold text-2xl ">{{ data.title }}</h2>
-      <div class="">{{ data.description }}</div>
+      <div class="mt-4">{{ data.description }}</div>
     </div>
     <button
       @click="$router.push(data.url)"
