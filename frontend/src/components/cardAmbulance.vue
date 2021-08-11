62a7e990fd74382d106d061d7dfe66744c9c5bdb
Rengga Prakoso Nugroho <rengga.prakoso@gmail.com>
2021-08-12 00:59:13 +0700

    feat: layout changes like rujukan RS

--- frontend/src/components/cardAmbulance.vue
+++ frontend/src/components/cardAmbulance.vue
@@ -1,30 +1,23 @@
 <template>
-  <div>
-    <div class="bg-white p-5 px-4 my-6 mx-4 shadow-sm rounded">
-      <div class="mx-2 my-2 p-2">
-        <div class="font-bold text-lg">
-         {{ data.ambulan }}
-        </div>
-        <span>{{ data.hotline }}</span>
-      </div>
-      <button @click="hubungi" class="m-2 py-2 px-5 border rounded-full">
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          class="inline icon icon-tabler icon-tabler-phone"
-          width="24"
-          height="24"
-          viewBox="0 0 24 24"
-          stroke-width="2"
-          stroke="currentColor"
-          fill="none"
-          stroke-linecap="round"
-          stroke-linejoin="round"
-        >
-          <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
-          <path
-            d="M5 4h4l2 5l-2.5 1.5a11 11 0 0 0 5 5l1.5 -2.5l5 2v4a2 2 0 0 1 -2 2a16 16 0 0 1 -15 -15a2 2 0 0 1 2 -2"
-          ></path>
-        </svg>
+  <div
+    :id="stringtokebabcase(data.ambulan)"
+    class="bg-white py-2 px-2 pb-4 shadow-sm rounded flex-col border-2 border-gray-200"
+  >
+    <div id="informasi-layanan" class="flex flex-col gap-2">
+      <h2 id="nama-layanan-ambukan" class="text-xl font-bold">
+        {{ data.ambulan }}
+      </h2>
+      <p id="hotline-rumahsakit" class="text-md">{{ data.hotline }}</p>
+    </div>
+    <div
+      id="informasi-kontak"
+      class="flex flex-col gap-1 border-t-2 border-gray-200 mt-4 pt-4"
+    >
+      <button
+        @click="hubungi"
+        id="hotline"
+        class="mt-2 rounded bg-blue-400 text-white font-bold py-3"
+      >
         Hubungi
       </button>
     </div>
@@ -33,14 +26,17 @@
 
 <script>
 export default {
-    props: ['data'],
-    methods:{
-        hubungi(){
-            window.location.href = `tel:${this.data.hotline}`
-        }
-    }
+  props: ["data"],
+  methods: {
+    hubungi() {
+      window.location.href = `tel:${this.data.hotline}`;
+    },
+    stringtokebabcase(str) {
+      let stripped = str.replace(/\./g, "");
+      return stripped.replace(/\s/g, "-").toLowerCase();
+    },
+  },
 };
 </script>
 
-<style>
-</style>
+<style></style>
