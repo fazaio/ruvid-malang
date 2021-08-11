8321b930633b008da6d59580ba853046a2627510
Rengga Prakoso Nugroho <rengga.prakoso@gmail.com>
2021-08-12 01:40:14 +0700

    feat: layout changes like rujukan RS

--- frontend/src/components/cardDarah.vue
+++ frontend/src/components/cardDarah.vue
@@ -1,61 +1,44 @@
 <template>
-  <div>
-    <div class="bg-white p-5 px-4 my-6 mx-4 shadow-sm rounded">
-      <div class="text-center">
-        Golongan Darah
-        <div class="p-4 text-5xl">{{ golongan }}</div>
-      </div>
-      <div class=" overflow-auto border border-blue-100 px-3 m-1 my-2 rounded bg-blue-50 py-1 " >
+  <div
+    :id="`stok-darah-`+golongan"
+    class="bg-white py-2 px-2 pb-4 shadow-sm rounded flex-col border-2 border-gray-200"
+  >
+    <h3 class="text-center">
+      Golongan Darah
+      <div class="p-4 text-5xl">{{ golongan }}</div>
+    </h3>
+    <div id="stok-darah">
+      <div
+        id="label-tabel"
+        class="overflow-auto border border-blue-100 px-3 m-1 my-2 rounded bg-blue-50 py-1 "
+      >
         <div class="font-bold float-left">Keterangan</div>
         <div class="font-bold float-right">Stock</div>
       </div>
-      <div>
-        <div v-for="row in data" :key="row.no">
-          <div class=" overflow-auto border border-gray-100 px-3 m-1 my-2 rounded bg-gray-50 py-1 " >
-            <div class="float-left">{{ row.produk }}</div>
-            <div class="float-right">{{ row[golongan] }}</div>
-          </div>
+      <div :id="`stokdarah-` + stringtokebabcase(row.produk)" v-for="row in data" :key="row.no">
+        <div
+          class=" overflow-auto border border-gray-100 px-3 m-1 my-2 rounded bg-gray-50 py-1 "
+        >
+          <div class="float-left">{{ row.produk }}</div>
+          <div class="float-right">{{ row[golongan] }}</div>
         </div>
       </div>
-      <button @click="hubungi" class="m-2 py-2 px-5 border rounded-full">
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          class="inline icon icon-tabler icon-tabler-phone"
-          width="24"
-          height="24"
-          viewBox="0 0 24 24"
-          stroke-width="2"
-          stroke="currentColor"
-          fill="none"
-          stroke-linecap="round"
-          stroke-linejoin="round"
-        >
-          <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
-          <path
-            d="M5 4h4l2 5l-2.5 1.5a11 11 0 0 0 5 5l1.5 -2.5l5 2v4a2 2 0 0 1 -2 2a16 16 0 0 1 -15 -15a2 2 0 0 1 2 -2"
-          ></path>
-        </svg>
+    </div>
+    <div
+      id="informasi-kontak"
+      class="flex flex-col gap-1 border-t-2 border-gray-200 mt-4 pt-4"
+    >
+      <button
+        @click="hubungi"
+        id="hotline"
+        class="mt-2 rounded bg-blue-400 text-white font-bold py-3"
+      >
         Hubungi
       </button>
-      <button @click="rute" class="m-2 py-2 px-5 border rounded-full">
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          class="inline icon icon-tabler icon-tabler-map-pin"
-          width="24"
-          height="24"
-          viewBox="0 0 24 24"
-          stroke-width="2"
-          stroke="currentColor"
-          fill="none"
-          stroke-linecap="round"
-          stroke-linejoin="round"
-        >
-          <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
-          <circle cx="12" cy="11" r="3"></circle>
-          <path
-            d="M17.657 16.657l-4.243 4.243a2 2 0 0 1 -2.827 0l-4.244 -4.243a8 8 0 1 1 11.314 0z"
-          ></path>
-        </svg>
+      <button
+        @click="rute"
+        class="mt-2 rounded border-2 border-blue-400 text-blue-400 py-3"
+      >
         Rute
       </button>
     </div>
@@ -72,9 +55,12 @@ export default {
     rute() {
       window.location.href = "https://goo.gl/maps/biwgFQvVixqmeR127";
     },
+    stringtokebabcase(str) {
+      let stripped = str.replace(/\./g, "");
+      return stripped.replace(/\s/g, "-").toLowerCase();
+    },
   },
 };
 </script>
 
-<style>
-</style>
+<style></style>
--- frontend/src/views/stokdarah.vue
+++ frontend/src/views/stokdarah.vue
@@ -1,38 +1,54 @@
 <template>
-  <div>
-    <menubar />
-    <div class="container mx-auto">
-      <div class="text-center">
-        <div class="font-bold">Stok Darah UTD PMI Kota Malang</div>
-        <div class="text-sm">
-          Jl. Buring No.10, Oro-oro Dowo, Kec. Klojen, Kota Malang, Jawa Timur
-          65119
+  <section id="section-stok-darah" class="px-8 mt-8 md:mx-24">
+    <!-- Last Update Section -->
+    <h2 class="pt-2 text-lg font-bold">Stok Darah UTD PMI Kota Malang</h2>
+    <p class="pt-2 text-sm">
+      Jl. Buring No.10, Oro-oro Dowo, Kec. Klojen, Kota Malang, Jawa Timur 65119
+    </p>
+    <a class="pt-2 text-sm underline text-blue-400" href="tel:+62341325443"
+      >Hubungi UTD PMI</a
+    >
+
+    <!-- Card Area -->
+    <section
+      id="stokdarah-card-group"
+      class="mt-8 grid gap-6 grid-flow-row grid-cols-1 md:grid-cols-2 lg:grid-cols-4"
+    >
+      <card-darah
+        v-for="(row, id) in loopGolongan"
+        :key="id"
+        :data="data[0]"
+        :golongan="row"
+      />
+    </section>
+
+    <!-- Disclaimer Data -->
+    <section
+      id="disclaimer-data"
+      class="border-red-500 border-2 my-4 p-4 bg-white"
+    >
+      <p class="text-red-400 font-bold text-xl">Penting!</p>
+      <div>
+        <p class="mt-2">
+          Data diatas dapat berubah sewaktu-waktu, cek pada halaman resmi untuk
+          informasi lebih lanjut.
+        </p>
+        <div class="mt-2">
+          <p class="font-bold text-md">Sumber Data:</p>
+          <a
+            class="underline text-blue-500"
+            href="http://ayodonor.pmi.or.id/stokdarah.php?module=3573"
+          >
+            Stok Darah UTD PMI Kota Malang (http://pmi.or.id)</a
+          >
         </div>
-        <div class="text-sm">+62341325443</div>
+        <p class="mt-4 text-sm">Last update: {{ data[1][0].cron_at }}</p>
       </div>
-      <div class="block md:grid lg:grid xl:grid grid-cols-2 gap-6">
-        <template v-for="(row, id) in loopGolongan" :key="id">
-          <card-darah :data="data[0]" :golongan="row" />
-        </template>
-      </div>
-      <div class="text-center">
-        <div class="py-2 text-sm">Last update: {{ data[1][0].cron_at }}</div>
-      </div>
-      <div class="mt-3 px-4 py-2 mx-4 bg-white">
-        <b class="text-red-400">Penting!</b>
-        <div>
-          Data Diatas Dapat <b>Berubah</b> sewaktu-waktu, cek pada halaman resmi
-          untuk informasi lebih lanjut. <br />
-          <b>Sumber Data:</b>
-          http://ayodonor.pmi.or.id/stokdarah.php?module=3573
-        </div>
-      </div>
-    </div>
-  </div>
+    </section>
+  </section>
 </template>
 
 <script>
-import menubar from "../components/menubar.vue";
 import cardDarah from "../components/cardDarah.vue";
 export default {
   data() {
@@ -41,7 +57,7 @@ export default {
       loopGolongan: ["A+", "B+", "O+", "AB+", "A-", "B-", "O-", "AB-"],
     };
   },
-  components: { menubar, cardDarah },
+  components: { cardDarah },
   created() {
     this.axios
       .get("/stokdarah")
@@ -51,5 +67,4 @@ export default {
 };
 </script>
 
-<style>
-</style>
+<style></style>
